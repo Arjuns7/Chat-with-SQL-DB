26c00f92f67db3a50641d359fb0baa63bee3b2f5
Arjun S <arjunjs.m8@gmail.com>
2025-05-12 19:25:53 +0530

    sql chat

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if not db_uri:
 if not api_key:
     st.info("please add the GROQ api key")
 
-llm = ChatGroq(api_key=groq_api_key, model_name="Llama-8b-8192",streaming=True)
+llm = ChatGroq(api_key=groq_api_key, model_name="Llama3-8b-8192",streaming=True)
 
 @st.cache_resource(ttl="2h")
 def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
@@ -50,7 +50,7 @@ def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysq
         dbfilepath = (Path(__file__).parent/"student.db")
         print(dbfilepath)
         creator = lambda:sqlite3.connect(f"file:{dbfilepath}?mode=ro",uri=True)
-        return SQLDatabase(create_engine("sqllite:///",creator=creator))
+        return SQLDatabase(create_engine("sqlite:///",creator=creator))
     
     elif db_uri==MYSQL:
         if not (mysql_host and mysql_user and mysql_password and mysql_db):
@@ -65,3 +65,30 @@ else:
     db = configure_db(db_uri)
 
 
+toolkit = SQLDatabaseToolkit(db=db,llm=llm)
+
+agent = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role":"assistant","content":"How can i help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query= st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role":"user","content":user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
